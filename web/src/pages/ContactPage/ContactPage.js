f57ef0eef999181cfbc164c7effbfed778c3b9eb
mvd29 <mvd29@njit.edu>
2023-01-28 18:53:39 -0500

    make contact fields required

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,16 +1,24 @@
-import { Form, TextField, Submit } from '@redwoodjs/forms'
+import { Form, TextField, TextAreaField, Submit } from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 
 const ContactPage = () => {
-  return (
-    <>
-      <MetaTags title="Contact" description="Contact page" />
+  const onSubmit = (data) => {
+    console.log(data)
+  }
 
-      <Form>
-        <TextField name="input" />
-        <Submit>Save</Submit>
-      </Form>
-    </>
+  return (
+    <Form onSubmit={onSubmit}>
+      <label htmlFor="name">Name</label>
+      <TextField name="name" required />
+
+      <label htmlFor="email">Email</label>
+      <TextField name="email" required />
+
+      <label htmlFor="message">Message</label>
+      <TextAreaField name="message" required />
+
+      <Submit>Save</Submit>
+    </Form>
   )
 }
 
