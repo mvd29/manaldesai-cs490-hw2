55928bbf42a65b7bb2e38dbfc6ce8363c688ede1
mvd29 <mvd29@njit.edu>
2023-01-28 18:43:18 -0500

    got homepage to work back and forth
    updated all of the files so homepage can go back and forth

--- web/src/pages/ArticlePage/ArticlePage.js
+++ web/src/pages/ArticlePage/ArticlePage.js
@@ -1,17 +1,13 @@
 import { MetaTags } from '@redwoodjs/web'
 
-const ArticlePage = () => {
+import ArticleCell from 'src/components/ArticleCell'
+
+const ArticlePage = ({ id }) => {
   return (
     <>
       <MetaTags title="Article" description="Article page" />
 
-      <h1>ArticlePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ArticlePage/ArticlePage.js</code>
-      </p>
-      <p>
-        My default route is named <code>article</code>, link to me with `
-      </p>
+      <ArticleCell id={id} rand={Math.random()} />
     </>
   )
 }
